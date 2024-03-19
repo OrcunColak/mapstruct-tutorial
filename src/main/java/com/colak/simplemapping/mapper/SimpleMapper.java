03d7ccf162bd10fe49e9f35c8bc8f50613d31db9
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-19 09:26:10 +0300

    mapper for list of items

--- src/main/java/com/colak/simplemapping/mapper/SimpleMapper.java
+++ src/main/java/com/colak/simplemapping/mapper/SimpleMapper.java
@@ -6,12 +6,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface SimpleMapper {
     SimpleMapper INSTANCE = Mappers.getMapper(SimpleMapper.class);
 
+    // We can convert a single item
     SimpleModel fromDto(SimpleDTO addressDTO);
 
+    // We can convert a list of items
+    List<SimpleModel> fromDtoList(List<SimpleDTO> addressDTOList);
+
+    // We can update model from DTO
     SimpleModel update(SimpleDTO addressDTO, @MappingTarget SimpleModel address);
 
 }
--- src/test/java/com/colak/simplemapping/mapper/SimpleMapperTest.java
+++ src/test/java/com/colak/simplemapping/mapper/SimpleMapperTest.java
@@ -5,13 +5,15 @@ import com.colak.simplemapping.dto.SimpleDTO;
 import com.colak.simplemapping.model.SimpleModel;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class SimpleMapperTest {
 
     @Test
-    void testMapper() {
+    void fromDto() {
         var simpleDTO = new SimpleDTO("Main Street", "New York");
 
         var simpleMapper = SimpleMapper.INSTANCE;
@@ -20,7 +22,25 @@ class SimpleMapperTest {
     }
 
     @Test
-    void testUpdate() {
+    void fromDtoList() {
+        var simpleDTO1 = new SimpleDTO("Main Street1", "New York1");
+        var simpleDTO2 = new SimpleDTO("Main Street2", "New York2");
+        List<SimpleDTO> dtoList = List.of(simpleDTO1, simpleDTO2);
+
+        var simpleMapper = SimpleMapper.INSTANCE;
+        List<SimpleModel> simpleModelList = simpleMapper.fromDtoList(dtoList);
+        assertEquals(2, simpleModelList.size());
+
+
+        SimpleModel first = simpleModelList.getFirst();
+        assertEquals("Main Street1", first.getStreet());
+
+        SimpleModel last = simpleModelList.getLast();
+        assertEquals("Main Street2", last.getStreet());
+    }
+
+    @Test
+    void update() {
         var simpleDTO = new SimpleDTO("Main Street", "New York");
         SimpleModel simpleModel = new SimpleModel("Broadway", "New York");
 
